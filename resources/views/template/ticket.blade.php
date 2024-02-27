febc44c06f9fc1168e8997a6aae3178c27155025
Dzann <gidzanmuh@gmail.com>
2024-02-27 23:24:53 +0700

    Fixing ticket PDF

--- resources/views/template/ticket.blade.php
+++ resources/views/template/ticket.blade.php
@@ -1,45 +1,54 @@
 <!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ticket</title>
 </head>
 <style>
     body {
         font-family: 'Arial', sans-serif;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
         margin: 0;
+        padding: 0;
     }
 
     .invoice-container {
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+        height: 100vh;
     }
 
     .invoice-card {
         border: 1px solid black;
         border-radius: 10px;
-        /* Sudut bulat */
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         width: 400px;
         padding: 20px;
-        margin-bottom: 20px;
-        /* background: linear-gradient(135deg, #8e44ad, #3498db);
-        color: white; */
-        
+        margin-bottom: 100px;
     }
 
     .invoice-header {
         text-align: center;
-        margin-top: -20px;
-        margin-left: 100px;
+        margin-bottom: 20px;
     }
-    .invoice-header b{
-        color: red;
+
+    .invoice-header img {
+        width: 25%;
+        vertical-align: middle;
+        margin-right: 10px;
+    }
+
+    .invoice-header h2 {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .invoice-header b {
+        color: red
     }
 
     hr {
@@ -54,36 +63,38 @@
     .invoice-details strong {
         margin-right: 5px;
         width: 130px;
-        /* Menyesuaikan lebar strong untuk sejajar */
         display: inline-block;
-        /* Menjadikan strong sebagai elemen inline-block */
     }
-
-
+    .page-break {
+    page-break-after: always;
+}   
 </style>
 
 <body>
-
     <div class="invoice-container">
         @foreach ($tickets as $ticket)
-        <div class="invoice-card">
-            <div class="invoice-header">
-                <img src="img/logo1.png" width="15%" alt="" style="margin-right: 25em"> 
-                <h2>| AniPlex Cinema | <b>{{ $ticket->seat }} </b></h2>
+            <div class="invoice-card">
+                <div class="invoice-header">
+                    <table>
+                        <tr>
+                            <td><img src="img/logo1.png" alt="AniPlex Cinema">
+                                <h2>| AniPlex Cinema | <b>{{ $ticket->seat }}</b></h2>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <hr>
+                <div class="invoice-details">
+                    <p><strong>Tanggal</strong>: {{ $ticket->created_at->format('Y-m-d') }}</p>
+                    <p><strong>Film</strong>: {{ $purchase->movie->name }}</p>
+                    <p><strong>Waktu</strong>: {{ $purchase->time }}</p>
+                    <p><strong>Seats</strong>: {{ $ticket->seat }}</p>
+                    <p><strong>Invoice</strong>: {{ $ticket->code }}</p>
+                </div>
             </div>
-            <hr>
-            <div class="invoice-details">        
-                <p><strong>Tanggal</strong>: {{ $ticket->created_at->format('Y-m-d')}}</p>
-                <p><strong>Film</strong>: {{ $purchase->movie->name }}</p>
-                <p><strong>Waktu</strong>: {{ $purchase->time }}</p>
-                <p><strong>Seats</strong>: {{ $ticket->seat }}</p>
-                <p><strong>Invoice</strong>: {{ $ticket->code }} </p>
-            </div>
-        </div>
+            {{-- <div class="page-break"></div> --}}
         @endforeach
     </div>
-    
-
 </body>
 
 </html>
